2f7648dcadf7b32e4b327f807f9efadf6f776344
Akira Kudo <akirakudo901@gmail.com>
2026-07-08 17:42:15 -0700

    feat : update sophistication of ghost by adding a "lookahead" parameter

--- src/action_requester/ai.py
+++ src/action_requester/ai.py
@@ -15,13 +15,18 @@ class CoordMatchGhostAI:
         if "enemy_id" not in context:
             raise Exception("Pass the enemy_id when requesting action to a ghost/enemy.")
         enemy_id: int = context["enemy_id"]
+        lookahead_size = context.get("lookahead_size", 0)
+        
         myself = observation.enemies[enemy_id]
         player = observation.player
 
         m_x, m_y = myself.coord
         p_x, p_y = player.coord
+        # compute "goal" location based on player position and direction
+        p_dir_x, p_dir_y = player.dir.delta()
+        g_x, g_y = p_x + lookahead_size * p_dir_x, p_y + lookahead_size * p_dir_y
         # check general direction of player relative to current position
-        d_x, d_y = (p_x - m_x), (p_y - m_y)
+        d_x, d_y = (g_x - m_x), (g_y - m_y)
         x_dir = Direction.delta_to_dir((d_x, 0))
         y_dir = Direction.delta_to_dir((0, d_y))
         # move in first direction if possible
