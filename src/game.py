c83f884f4d00e7fdc4ff05ad277b5346847fe77d
Akira Kudo <akirakudo901@gmail.com>
2026-07-18 23:57:19 -0700

    feat (game) : incorporate super pacman and ghost death

--- src/game.py
+++ src/game.py
@@ -11,7 +11,7 @@ from dataclasses import dataclass
 from copy import deepcopy
 
 from src.entity import Entity, Player, Enemy
-from src.map import Direction, Map
+from src.map import Coord, Direction, Map
 
 from src.terminal_visualizer import TerminalGameVisualizer
 from src.visualizer import GameVisualizer
@@ -26,13 +26,30 @@ class Observation:
     enemies: list[Enemy]
 
 PELLET_SCORE = 1
+POWER_PELLET_SCORE = 5
 
 class GameMap:
 
-    def __init__(self, map: Map, player: Player, enemies: list[Enemy]) -> None:
+    def __init__(
+        self, 
+        map: Map, 
+        player: Player, 
+        enemies: list[Enemy],
+        super_pacman_len: int | None=None,
+        death_lens: list[int|None] | None=None
+        ) -> None:
+        """Overwrites super_pacman_len of player & death_len for each enemy if provided."""
         self.map = map
         self.player = player
         self.enemies = enemies
+
+        if super_pacman_len:
+            self.player.set_super_pacman_length(super_pacman_len)
+        
+        if death_lens:
+            for enemy, d_len in zip(self.enemies, death_lens):
+                if d_len:
+                    enemy.set_death_length(d_len)
         
         # check the map allows these starting positions
         self._validate_coord_on_map(self.player)
@@ -60,6 +77,22 @@ class GameMap:
         self.player = deepcopy(self.init_player_state)
         self.enemies = [deepcopy(e) for e in self.init_enemy_states]
     
+    def _identify_enemy_player_collision(
+        self, 
+        prev_player_coord: Coord,
+        prev_enemy_coords: list[Coord]
+        ) -> list[bool]:
+        """Identify enemy-player collision: enemy on top of the player, or they passed through each other."""
+        ret = []
+        for enemy, prev_enemy_coord in zip(self.enemies, prev_enemy_coords):
+            has_collided = not enemy.is_dead() and (
+                self.player.coord == enemy.coord or \
+                self.player.coord == prev_enemy_coord and prev_player_coord == enemy.coord
+                )
+            ret.append(has_collided)
+        return ret
+
+    
     def step(self, player_action: Action, enemy_actions: list[Action]) -> tuple[Observation, bool, bool]:
         def _lose():
             self.done, self.won = True, False
@@ -73,27 +106,49 @@ class GameMap:
         # move player
         self.player.move(player_action, self.map)
         # if pellet is at the new location, consume it
-        has_pellet = self.map.have_pellets([self.player.coord])[0]
-        if has_pellet:
-            self.map.consume_pellets([self.player.coord])
+        has_normal_pellet = self.map.have_normal_pellets([self.player.coord])[0]
+        has_power_pellet  =  self.map.have_power_pellets([self.player.coord])[0]
+
+        if has_normal_pellet:
             self.score += PELLET_SCORE
         
-        # move enemies
+        if has_power_pellet:
+            self.score += POWER_PELLET_SCORE
+            # also refill super pacman mode
+            self.player.start_super_pacman_mode()
+        
+        self.map.consume_pellets([self.player.coord])
+        
+        # move enemies if not deead
         for enemy, e_action in zip(self.enemies, enemy_actions):
-            enemy.move(e_action, self.map)
+            if not enemy.is_dead():
+                enemy.move(e_action, self.map)
         
         # create the observation
         obs = self.get_observation()
         
-        # if enemies are on top of the player, or they passed through each other, lose
-        for enemy, prev_enemy_coord in zip(self.enemies, prev_enemy_coords):
-            if (self.player.coord == enemy.coord or \
-                self.player.coord == prev_enemy_coord and prev_player_coord == enemy.coord):
+        # identify collisions
+        collisions = self._identify_enemy_player_collision(prev_player_coord, prev_enemy_coords)
+        # if not in super pacman mode, lose if we collided
+        if not self.player.is_super_pacman_mode():
+            if any(collisions): 
                 _lose()
                 return obs, self.done, self.won
+        # otherwise, enemies that collided are 'dead'
+        else:
+            for enemy, collided in zip(self.enemies, collisions):
+                if collided:
+                    enemy.kill()
+        
         # if there's no more pellet otherwise, win
         if self.map.have_no_pellet():
             _win()
+        
+        # tick both player and enemies (e.g. super pacman mode countdown)
+        self.player.tick()
+        for enemy in self.enemies: 
+            enemy.tick()
+
         return obs, self.done, self.won
     
     def get_observation(self) -> Observation:
