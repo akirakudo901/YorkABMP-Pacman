3a371409bc185b8e541869d7c4d7e201bd76523e
Akira Kudo <akirakudo901@gmail.com>
2026-07-19 11:36:02 -0700

    feat : update rules to cleanly separate the different mode logics and the switching between modes from each other

--- src/rules.py
+++ src/rules.py
@@ -5,13 +5,42 @@ import random
 from src.game import Observation
 from src.map import Direction
 
+RUSH_MODE_NAME = "rush"
+IN_DANGER_MODE_NAME = "in_danger"
+BIG_PACMAN_MODE_NAME = "big_pacman"
+
 # Takes in an observation, and outputs a Direction object (random)
 # See the ```game.py``` file for the definition & details of Observation (a bit complicated).`
 # A Direction object is one of: 
 # - Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT
 # - Direction.NEUTRAL for no input / motion
 def request_action(observation: Observation) -> Direction:
+    player = observation.player
 
+    player_mode = player.get_mode()
+    
+    # initial state: unset, left as empty. Then, set to default which is rush
+    if player_mode == "":
+        player.set_mode(RUSH_MODE_NAME)
+        player_mode = player.get_mode()
+
+    # Switch between "modes" based on triggers!
+    
+
+    # we run the logic under the different states
+    if player_mode == RUSH_MODE_NAME:
+        action = rush_state_logic(observation)
+    elif player_mode == IN_DANGER_MODE_NAME:
+        action = in_danger_state_logic(observation)
+    elif player_mode == BIG_PACMAN_MODE_NAME:
+        action = super_pacman_state_logic(observation)
+    else:
+        raise Exception(f"Player mode {player_mode} isn't implemented.")
+    
+    return action
+
+
+def rush_state_logic(observation: Observation) -> Direction:
     player = observation.player
     dir = player.dir
     target = dir.move_towards(player.coord)
@@ -34,3 +63,11 @@ def request_action(observation: Observation) -> Direction:
             return random.choice(valid_dirs)
         else:
             return random.choice(pellet_dirs)
+
+
+def in_danger_state_logic(observation: Observation) -> Direction:
+    return None
+
+
+def super_pacman_state_logic(observation: Observation) -> Direction:
+    return None
