826404af7b630457cd3a32f2c062b7d27cef2c79
Akira Kudo <akirakudo901@gmail.com>
2026-07-21 01:46:22 -0700

    feat : reflect the use of the shortest path map to the Ghost AI, and play

--- src/action_requester/ai.py
+++ src/action_requester/ai.py
@@ -2,8 +2,10 @@
 A simple rule-based AI for the ghosts and player.
 """
 
+import random
 from typing import TYPE_CHECKING
 
+from src.action_requester.precompute_shortest_path import GroupedShortestPathMap
 from src.map import Direction
 
 if TYPE_CHECKING:
@@ -42,6 +44,55 @@ class CoordMatchGhostAI:
             return x_dir
         # otherwise move in second direction
         return y_dir
+
+
+class ShortestPathGhostAI:
+
+    def __init__(self, shortest_path_map: GroupedShortestPathMap) -> None:
+        self.shortest_path_map = shortest_path_map
+
+    def request_action(self, observation: "Observation", context: dict) -> "Action":
+        if "enemy_id" not in context:
+            raise Exception("Pass the enemy_id when requesting action to a ghost/enemy.")
+        enemy_id: int = context["enemy_id"]
+        lookahead_size = context.get("lookahead_size", 0)
+        
+        myself = observation.enemies[enemy_id]
+        player = observation.player
+
+        # simple reeturn if dead or every odd frame for super countdown
+        if myself.is_dead() or player.get_super_pacman_countdown() % 2 == 1:
+            return Direction.NEUTRAL
+        
+        # compute "goal" location: furthest valid point from player.coord in (player.dir or -player.dir)
+        current, direction = player.coord, player.dir
+        if lookahead_size < 0:
+            direction = direction.opposite()
+        steps = abs(lookahead_size)
+
+        # Step out up to 'steps', or until hit a wall
+        furthest = current
+        for _ in range(steps):
+            next_coord = direction.move_towards(furthest)
+            if not observation.map.can_move(next_coord):
+                break
+            furthest = next_coord
+        goal_coord = furthest
+        
+        # query and return the best direction for fastest approach to the player
+        best_dir_towards_player = self.shortest_path_map.query_direction(goal_coord, myself.coord)
+        if player.is_super_pacman_mode():
+            valid_dirs = [
+                d for d in Direction 
+                if d not in (best_dir_towards_player, Direction.NEUTRAL)
+            ]
+            if valid_dirs:
+                return random.choice(valid_dirs)
+            else:
+                return Direction.NEUTRAL
+        else:
+            return best_dir_towards_player
+
 
 from src.rules import request_action as request_action_phoebe
 
--- src/play.py
+++ src/play.py
@@ -2,10 +2,14 @@
 File for playing a game!
 """
 
+import json
+from pathlib import Path
+
+from src.action_requester.precompute_shortest_path import GroupedShortestPathMap
 from src.entity import Enemy, Player
 from src.game import game_loop
 from src.map import Coord, Map
-from src.action_requester.ai import CoordMatchGhostAI, PhoebePlayerAI
+from src.action_requester.ai import CoordMatchGhostAI, PhoebePlayerAI, ShortestPathGhostAI
 from src.action_requester.controller import KeyboardController
 
 KEYBOARD_TIMEOUT_MS = 200
@@ -14,16 +18,37 @@ VIS_FORMAT = "terminal"
 
 DEFAULT_ENEMY_LOOKAHEAD_SIZES = [-3, 3, 1, -1]
 
+PRECOMPUTED_DIR = r"precomputed_shortest_path"
+
 def make_game(
     map_file_path: str, 
     player_init_coord: Coord,
     enemy_init_coords: list[Coord],
     enemy_lookahead_sizes: list[int]=DEFAULT_ENEMY_LOOKAHEAD_SIZES,
-    control_player: bool=False
+    control_player: bool=False,
+    use_best_path_ghost_ai: bool=True
     ):
     with open(map_file_path, 'r') as f:
         map_ascii = f.read()
     map = Map.map_from_ascii(map_ascii)
+
+    if use_best_path_ghost_ai:
+        precomputed_path = Path(PRECOMPUTED_DIR) / f"{Path(map_file_path).stem}_precomputed.json"
+        # if pre-existing, load
+        if precomputed_path.is_file():
+            with open(precomputed_path, 'r') as f:
+                content = json.load(f)
+            precomputed_map = GroupedShortestPathMap.from_json(content)
+        else:
+            precomputed_map = GroupedShortestPathMap(map)
+            precomputed_json = precomputed_map.to_json()
+            with open(precomputed_path, 'w') as f:
+                json.dump(precomputed_json, f)
+        
+        ghost_ai_cls = lambda: ShortestPathGhostAI(precomputed_map)
+   
+    else:
+        ghost_ai_cls = CoordMatchGhostAI
 
     if control_player:
         player = Player(
@@ -36,7 +61,7 @@ def make_game(
     enemy_lookahead_sizes = enemy_lookahead_sizes + [0] * len(enemy_init_coords)
     enemy_lookahead_sizes = enemy_lookahead_sizes[:len(enemy_init_coords)]
     enemies = [
-        Enemy(init_coord=coord, action_requester=CoordMatchGhostAI(), 
+        Enemy(init_coord=coord, action_requester=ghost_ai_cls(), 
               enemy_id=i, lookahead_size=lsz)
         for i, (coord, lsz) in enumerate(zip(enemy_init_coords, enemy_lookahead_sizes))
     ]
